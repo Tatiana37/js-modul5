654b9b9d2b72a8adc7026f5e580eb3bad9c0904c
Tetiana Blizniukova <tatbliz30@gmail.com>
2021-06-22 21:55:52 +0300

    добавила решение задач автопроверки 5го модуля

--- js/script.js
+++ js/script.js
@@ -182,3 +182,417 @@
 
 //  6
 
+// function Storage(items){
+// this.items = items;
+// } 
+// Storage.prototype.getItems = function(){
+// return this.items;
+// };
+// Storage.prototype.addItem = function(newItem){
+// return this.items.push(newItem);
+
+// };
+// Storage.prototype.removeItem = function (item) {
+//     const itemIndex = this.items.indexOf(item);
+// return this.items.splice(itemIndex, 1);
+// };
+
+
+// // Пиши код выше этой строки
+// const storage = new Storage(['Нанитоиды', 'Пролонгер', 'Антигравитатор']);
+// console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор"]
+// storage.addItem('Дроид');
+// console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор", "Дроид"]
+// storage.removeItem('Пролонгер');
+// console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Дроид"]
+
+
+//  7
+
+// function StringBuilder(baseValue){
+// this.value = baseValue;
+// }
+// StringBuilder.prototype.getValue = function(){
+// return this.value;
+// };
+// StringBuilder.prototype.padEnd = function (str){
+// return this.value = this.value + str;
+// } 
+// StringBuilder.prototype.padStart = function(str){
+// return this.value = str + this.value;
+// }
+// StringBuilder.prototype.padBoth = function(str){
+// return this.value = str + this.value + str;
+// }
+// // Пиши код выше этой строки
+// const builder = new StringBuilder('.');
+// console.log(builder.getValue()); // '.'
+// builder.padStart('^');
+// console.log(builder.getValue()); // '^.'
+// builder.padEnd('^');
+// console.log(builder.getValue()); // '^.^'
+// builder.padBoth('=');
+// console.log(builder.getValue()); // '=^.^='
+
+
+//  8
+
+//  теория (Объявление класса)
+// class User {
+//   // Тело класса
+// }
+
+// const mango = new User();
+
+//  задача
+// class Car {
+
+// };
+
+
+//  9
+// //  теория (Конструктор класса)
+// class User {
+//   // Необязательное объявление публичных свойств
+//   name;
+//   email;
+
+//   // Аналог функции-конструктора
+//   constructor({ name, email }) {
+//     // Инициализация объявленных свойств
+//     this.name = name;
+//     this.email = email;
+//   }
+// }
+
+// const mango = new User({ name: 'Манго', email: 'mango@mail.com' });
+// console.log(mango);
+
+//  задача
+// class Car{
+//   brand;
+//   model;
+//   price;
+
+// constructor({brand, model, price}){
+//   this.brand = brand;
+//   this.model = model;
+//   this.price = price;
+// }
+// }
+// console.log(new Car({ brand: 'Audi', model: 'Q3', price: 36000}));
+// console.log(new Car({ brand: 'BMW', model: 'X5', price: 58900 }));
+// console.log(new Car({ brand: 'Nissan', model: 'Murano', price: 31700 }));
+
+
+//  10
+
+//  теория (Методы класса)
+// class User {
+//   constructor({ name, breed }) {
+//     this.name = name;
+//     this.breed = breed;
+//   }
+
+//   // Аналог User.prototype.getEmail
+//   getEmail() {
+//     return this.email;
+//   }
+
+//   // Аналог User.prototype.changeEmail
+//   changeEmail(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+
+//  задача
+// class Car {
+//   constructor({ brand, model, price }) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+//   getPrice(){
+//   return this.price;
+//   }
+//   changePrice(newPrice){
+
+//   return this.price = newPrice;
+//   }
+// }
+
+// console.log(Car.prototype.hasOwnProperty('getPrice'));
+// console.log(Car.prototype.hasOwnProperty('changePrice'));
+// console.log(new Car({ brand: 'Audi', model: 'Q3', price: 36000 }));
+// console.log(Car.prototype.getPrice());
+// console.log(Car.prototype.changePrice(35000));
+// console.log(Car.prototype.getPrice());
+
+
+
+//  11
+
+//  теория (Приватные свойства)
+// class User {
+//   // Необязательное объявление публичных свойств
+//   name;
+//   // Обязательное объявление приватных свойств
+//   #email;
+
+//   constructor({ name, email }) {
+//     this.name = name;
+//     this.#email = email;
+//   }
+
+//   getEmail() {
+//     return this.#email;
+//   }
+
+//   changeEmail(newEmail) {
+//     this.#email = newEmail;
+//   }
+// }
+
+// const mango = new User({ name: 'Манго', email: 'mango@mail.com' });
+
+// mango.changeEmail('mango@supermail.com');
+// console.log(mango.getEmail()); // mango@supermail.com
+// // console.log(mango.#email); // Будет ошибка, это приватное свойство
+
+//  задача
+// class Car {
+//   #brand;
+//   constructor({ brand, model, price }) {
+//     this.#brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+//   getBrand(){
+//     return this.#brand;
+//   }
+//   changeBrand(newBrand){
+//   return this.#brand = newBrand;
+//   }
+// }
+
+// console.log(new Car({ brand: 'Audi', model: 'Q3', price: 36000 }));
+// console.log(new Car({ brand: 'BMW', model: 'X5', price: 58900 }));
+// console.log(new Car({ brand: 'Nissan', model: 'Murano', price: 31700 }));
+// console.log(сar.getBrand());
+// console.log(сar.changeBrand('Honda'));
+
+
+//  12
+
+// class Storage{
+//   #items;
+//   constructor(items){
+//   this.#items = items;
+//   }
+
+// getItems() {
+//   return this.#items;
+// };
+// addItem(newItem) {
+//   this.#items.push(newItem);
+// };
+// removeItem(item) {
+//   const itemIndex = this.#items.indexOf(item);
+//   this.#items.splice(itemIndex, 1);
+// };
+// }
+// // Пиши код выше этой строки
+// const storage = new Storage(["Нанитоиды", "Пролонгер", "Антигравитатор"]);
+// console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор"]
+// storage.addItem("Дроид");
+// console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор", "Дроид"]
+// storage.removeItem("Пролонгер");
+// console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Дроид"]
+
+
+//  13
+
+// class StringBuilder {
+//   #value;
+//   constructor (baseValue){
+//   this.#value = baseValue;
+// }
+
+// getValue() {
+//   return this.#value;
+// };
+
+// padEnd(str) {
+//   this.#value += str;
+// };
+
+// padStart(str) {
+//   this.#value = str + this.#value;
+// };
+
+// padBoth(str) {
+//   this.padStart(str);
+//   this.padEnd(str);
+// };
+// }
+// // Пиши код выше этой строки
+// const builder = new StringBuilder('.');
+// console.log(builder.getValue()); // '.'
+// builder.padStart('^');
+// console.log(builder.getValue()); // '^.'
+// builder.padEnd('^');
+// console.log(builder.getValue()); // '^.^'
+// builder.padBoth('=');
+// console.log(builder.getValue()); // '=^.^='
+
+
+//  14
+
+// //  теория (Геттеры и сеттеры)
+// class User {
+//   name;
+//   #email;
+
+//   constructor({ name, email }) {
+//     this.name = name;
+//     this.#email = email;
+//   }
+
+//   // Геттер email
+//   get email() {
+//     return this.#email;
+//   }
+
+//   // Сеттер email
+//   set email(newEmail) {
+//     this.#email = newEmail;
+//   }
+// }
+// const mango = new User({ name: 'Манго', email: 'mango@mail.com' });
+// console.log(mango.email); // mango@mail.com
+// mango.email = 'mango@supermail.com';
+// console.log(mango.email); // mango@supermail.com
+
+
+// set email(newEmail) {
+//   if(newEmail === '') {
+//     console.log('Ошибка! Почта не может быть пустой строкой!');
+//     return;
+//   }
+
+//   this.#email = newEmail;
+// }
+
+//  задача
+// class Car {
+//   #model;
+//   #price;
+//   #brand;
+
+//   constructor({ brand, model, price }) {
+//     this.#brand = brand;
+//     this.#model = model;
+//     this.#price = price;
+//   }
+
+//   get brand() {
+//     return this.#brand;
+//   }
+//   set brand(newBrand) {
+//     this.#brand = newBrand;
+//   }
+
+//   get model() {
+//     return this.#model;
+//   }
+
+//  set model(newModel) {
+//     this.#model = newModel;
+//   }
+
+//   get price() {
+//     return this.#price;
+//   }
+
+//   set price(newPrice) {
+//     this.#price = newPrice;
+//   }
+// }
+
+
+//  15
+
+//  теория (Статические свойства)
+// class User {
+//   // Объявление и инициализация статического свойства
+//   static TYPES = {
+//     ADMIN: 'admin',
+//     EDITOR: 'editor',
+//   };
+//   #email;
+//   #type;
+
+//   constructor({ email, type }) {
+//     this.#email = email;
+//     this.#type = type;
+//   }
+
+//   get type() {
+//     return this.#type;
+//   }
+
+//   set type(newType) {
+//     if (User.TYPES[newType] === undefined) {
+//       console.log('Ошибка! Такого типа пользователя не существет');
+//       return;
+//     }
+
+//     this.#type = newType;
+//   }
+// }
+
+// const mango = new User({
+//   email: 'mango@mail.com',
+//   type: User.TYPES.ADMIN,
+// });
+
+// console.log(mango.TYPES); // undefined
+// console.log(User.TYPES); // { ADMIN: 'admin', EDITOR: 'editor' }
+
+// console.log(mango.type); // admin
+// mango.type = User.TYPES.EDITOR;
+// console.log(mango.type); // editor
+
+//  задача
+// class Car {
+//   // Пиши код ниже этой строки
+//     static MAX_PRICE = 50000;
+//     #price;
+
+//     constructor({ price }) {
+//     this.#price = price;
+//     }
+
+//     get price() {
+//         return this.#price;
+//     }
+
+//     set price(newPrice) {
+//     if (newPrice <= Car.MAX_PRICE){
+//         return this.#price = newPrice;
+//     }
+//     }
+//   // Пиши код выше этой строки
+// }
+
+// const audi = new Car({price: 35000});
+// console.log(audi.price); // 35000
+
+// audi.price = 49000;
+// console.log(audi.price); // 49000
+
+// audi.price = 51000;
+// console.log(audi.price); // 49000
+
+
+//  16
